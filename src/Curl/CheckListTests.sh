5383f063d09446221b122245dbd6611f2a7d50a7
lars <if24b115@technikum-wien.at>
2026-01-07 23:16:42 +0100

    Added more Curl Scripts

--- src/Curl/CheckListTests.sh
+++ src/Curl/CheckListTests.sh
@@ -44,22 +44,22 @@ delete() {
     -X DELETE "$1"
 }
 
-# =========================
-# REGISTRIEREN
-# =========================
-rm -f "$COOKIE_FILE"
-
-print_step "NEUEN Account erstellen"
-post "$BASE_URL/users/register" '{
-  "username": "larshosnedl",
-  "password": "test123"
-}'
-
-print_step "Account erstellen, dessen USERNAME VERGEBEN ist"
-post "$BASE_URL/users/register" '{
-  "username": "larshosnedl",
-  "password": "test123"
-}'
+## =========================
+## REGISTRIEREN
+## =========================
+#rm -f "$COOKIE_FILE"
+#
+#print_step "NEUEN Account erstellen"
+#post "$BASE_URL/users/register" '{
+#  "username": "larshosnedl",
+#  "password": "test123"
+#}'
+#
+#print_step "Account erstellen, dessen USERNAME VERGEBEN ist"
+#post "$BASE_URL/users/register" '{
+#  "username": "larshosnedl",
+#  "password": "test123"
+#}'
 
 
 # =========================
@@ -74,7 +74,7 @@ post "$BASE_URL/users/login" '{
 
 print_step "LOGIN mit GÜLTIGEN Zugangsdaten"
 post "$BASE_URL/users/login" '{
-  "username": "admin",
+  "username": "user1",
   "password": "test123"
 }'
 
@@ -85,7 +85,7 @@ print_step "Alle Media Einträge abrufen (Filme, Serien und Spiele)"
 get "$BASE_URL/media"
 
 print_step "Einen SPEZIFISCHEN Media Eintrag abrufen"
-get "$BASE_URL/media/aaaaaaaa-aaaa-aaaa-aaaa-aaaaaaaaaaaa"
+get "$BASE_URL/media/c56a6273-fc47-42e9-b72e-d54f90c5e88a"
 
 print_step "ALLE FILME"
 get "$BASE_URL/media/movies"
@@ -108,7 +108,7 @@ get "$BASE_URL/media/filter/release_year/2008"
 # KOMMENTAR TESTS
 # =========================
 print_step "Alle Kommentare zu einem Media Eintrag abrufen"
-get "$BASE_URL/comments/media/aaaaaaaa-aaaa-aaaa-aaaa-aaaaaaaaaaaa"
+get "$BASE_URL/comments/media/c56a6273-fc47-42e9-b72e-d54f90c5e88a"
 
 print_step "Alle Kommentare eines Benutzers abrufen"
 get "$BASE_URL/comments/users"
@@ -122,7 +122,7 @@ print_step "Eigenes Profil abrufen"
 get "$BASE_URL/profile/users"
 
 print_step "Profil eines bestimmten Users abrufen"
-get "$BASE_URL/profile/users/11111111-1111-1111-1111-111111111111"
+get "$BASE_URL/profile/users/57a41ed7-9b9d-442e-b972-7ab035cffc0a"
 
 
 
@@ -130,21 +130,21 @@ get "$BASE_URL/profile/users/11111111-1111-1111-1111-111111111111"
 # BEWERTUNG TESTS
 # =========================
 print_step "Einen Media Eintrag bewerten"
-post "$BASE_URL/ratings/media/aaaaaaaa-aaaa-aaaa-aaaa-aaaaaaaaaaaa" '{
+post "$BASE_URL/ratings/media/c56a6273-fc47-42e9-b72e-d54f90c5e88a" '{
   "stars": 3
 }'
 
 print_step "Alle Bewertungen eines bestimmten Media Eintrages abrufen"
-get "$BASE_URL/ratings/media/aaaaaaaa-aaaa-aaaa-aaaa-aaaaaaaaaaaa"
+get "$BASE_URL/ratings/media/c56a6273-fc47-42e9-b72e-d54f90c5e88a"
 
 print_step "Alle Bewertungen abrufen, die man selbst abgegeben hat"
 get "$BASE_URL/ratings/users"
 
 print_step "Alle Bewertungen abrufen, die ein bestimmter User abgegeben hat"
-get "$BASE_URL/ratings/users/22222222-2222-2222-2222-222222222222"
+get "$BASE_URL/ratings/users/130695af-3d51-45c4-b889-c1f1865afdae"
 
 print_step "Eine Bewertung löschen"
-delete "$BASE_URL/ratings/media/aaaaaaaa-aaaa-aaaa-aaaa-aaaaaaaaaaaa"
+delete "$BASE_URL/ratings/media/ac43d4c1-d0ea-4458-9b90-8968edf90e80"
 
 
 
@@ -153,15 +153,15 @@ delete "$BASE_URL/ratings/media/aaaaaaaa-aaaa-aaaa-aaaa-aaaaaaaaaaaa"
 # =========================
 
 print_step "Einen Media Eintrag kommentieren"
-post "$BASE_URL/comments/media/aaaaaaaa-aaaa-aaaa-aaaa-aaaaaaaaaaaa" '{
+post "$BASE_URL/comments/media/ac43d4c1-d0ea-4458-9b90-8968edf90e80" '{
   "comment_text": "Gefällt mir sehr gut! Spannend bis zum Ende."
 }'
 
 print_step "Einen Kommentar bestätigen"
-post "$BASE_URL/comments/media/aaaaaaaa-aaaa-aaaa-aaaa-aaaaaaaaaaaa"
+post "$BASE_URL/comments/media/ac43d4c1-d0ea-4458-9b90-8968edf90e80"
 
 print_step "Alle Kommentare eines bestimmten Media Eintrages abrufen"
-get "$BASE_URL/comments/media/aaaaaaaa-aaaa-aaaa-aaaa-aaaaaaaaaaaa"
+get "$BASE_URL/comments/media/ac43d4c1-d0ea-4458-9b90-8968edf90e80"
 
 print_step "Alle Kommentare abrufen, die man selbst geschrieben hat"
 get "$BASE_URL/comments/users"
@@ -172,13 +172,13 @@ get "$BASE_URL/comments/users"
 # =========================
 
 print_step "Einen Media Eintrag als Favorit hinzufügen"
-post "$BASE_URL/favorites/media/aaaaaaaa-aaaa-aaaa-aaaa-aaaaaaaaaaaa"
+post "$BASE_URL/favorites/media/ac43d4c1-d0ea-4458-9b90-8968edf90e80"
 
 print_step "Alle Favoriten des angemeldeten Benutzers anzeigen"
 get "$BASE_URL/favorites/users"
 
 print_step "Einen Media Eintrag aus den Favoriten entfernen"
-delete "$BASE_URL/favorites/media/aaaaaaaa-aaaa-aaaa-aaaa-aaaaaaaaaaaa"
+delete "$BASE_URL/favorites/media/e75e9532-f21c-47ae-92d7-b508cd785247"
 
 
 
--- src/Curl/SetupAccount.sh
+++ src/Curl/SetupAccount.sh
@@ -78,15 +78,71 @@ genres=(sci-fi drama rpg crime action sci-fi mystery western action rpg)
 age_restrictions=(12 16 18 16 18 12 16 18 12 18)
 release_years=(1999 2003 1989 2010 2022 2004 2018 1999 2000 2008)
 
-print_step "10 Media Einträge mit wechselnden Accounts erstellen"
+media_ids=(
+  c56a6273-fc47-42e9-b72e-d54f90c5e88a
+  0fb1c40e-fc70-414b-a209-9f64cf601802
+  e45ba185-4ca4-43b0-98ef-3bc6c3c8c6a3
+  5f6bd727-c6af-4a61-88e4-e37d644f75d1
+  167bd34f-0ea2-4a8e-8420-f8f4759d1a5a
+  e75e9532-f21c-47ae-92d7-b508cd785247
+  ac43d4c1-d0ea-4458-9b90-8968edf90e80
+  1a7202da-4527-4435-b1dd-f914a594e5a5
+  3c0bab8f-3da9-4fa3-b9f3-2c516134e4b7
+  f14f4a64-3dc6-4b00-bd59-0fc734f5b1a7
+)
+
+  comments_positive=(
+    "Absolut großartig!"
+    "Sehr empfehlenswert."
+    "Hat mir richtig gut gefallen."
+    "Top Umsetzung."
+  )
+
+  comments_negative=(
+    "Leider enttäuschend."
+    "Hatte mehr erwartet."
+    "Ziemlich langweilig."
+    "Nicht mein Geschmack."
+  )
+
+
+
+#print_step "10 Media Einträge mit wechselnden Accounts erstellen"
+#
+#for i in {0..9}; do
+#  user_index=$((i % 5))
+#  current_user=${users[$user_index]}
+#
+#  echo "==== LOGIN als $current_user ===="
+#
+#  # Cookies löschen, um neuen Login zu erzwingen
+#  rm -f "$COOKIE_FILE"
+#
+#  post "$BASE_URL/users/login" "{
+#    \"username\": \"$current_user\",
+#    \"password\": \"$password\"
+#  }"
+#
+#  echo "Erstelle Media Eintrag $((i+1)) als $current_user"
+#
+#  post "$BASE_URL/media/" "{
+#    \"title\": \"${titles[$i]}\",
+#    \"director\": \"${directors[$i]}\",
+#    \"description\": \"${descriptions[$i]}\",
+#    \"mediaType\": \"${mediaTypes[$i]}\",
+#    \"genre\": \"${genres[$i]}\",
+#    \"ageRestriction\": ${age_restrictions[$i]},
+#    \"releaseYear\": ${release_years[$i]}
+#  }"
+#
+#done
 
 for i in {0..9}; do
   user_index=$((i % 5))
   current_user=${users[$user_index]}
+  media_id=${media_ids[$i]}
 
   echo "==== LOGIN als $current_user ===="
-
-  # Cookies löschen, um neuen Login zu erzwingen
   rm -f "$COOKIE_FILE"
 
   post "$BASE_URL/users/login" "{
@@ -94,19 +150,44 @@ for i in {0..9}; do
     \"password\": \"$password\"
   }"
 
-  echo "Erstelle Media Eintrag $((i+1)) als $current_user"
+  # =========================
+  # BEWERTUNG
+  # =========================
+  stars=$((RANDOM % 5 + 1))
 
-  post "$BASE_URL/media/" "{
-    \"title\": \"${titles[$i]}\",
-    \"director\": \"${directors[$i]}\",
-    \"description\": \"${descriptions[$i]}\",
-    \"mediaType\": \"${mediaTypes[$i]}\",
-    \"genre\": \"${genres[$i]}\",
-    \"ageRestriction\": ${age_restrictions[$i]},
-    \"releaseYear\": ${release_years[$i]}
+  print_step "Einen Media Eintrag bewerten ($media_id)"
+
+  post "$BASE_URL/ratings/media/$media_id" "{
+    \"stars\": $stars
   }"
 
+  # =========================
+  # KOMMENTAR (max. 1 pro User & Media)
+  # =========================
+  if (( RANDOM % 2 )); then
+    if (( stars >= 3 )); then
+      comment=${comments_positive[$RANDOM % ${#comments_positive[@]}]}
+    else
+      comment=${comments_negative[$RANDOM % ${#comments_negative[@]}]}
+    fi
+
+    print_step "Einen Media Eintrag kommentieren ($media_id)"
+
+    post "$BASE_URL/comments/media/$media_id" "{
+      \"comment_text\": \"$comment\"
+    }"
+  fi
+
+  # =========================
+  # FAVORIT (nicht bei allen Medien)
+  # =========================
+  if (( RANDOM % 3 == 0 )); then
+    print_step "Einen Media Eintrag als Favorit hinzufügen ($media_id)"
+
+    post "$BASE_URL/favorites/media/$media_id" "{}"
+  fi
 done
+
 
 
 
